4fe92a2a22c3f5b55b6fb78f918a60278e276fbe
EC2 Default User <ec2-user@ip-172-31-13-110.ap-northeast-2.compute.internal>
2025-01-27 13:49:31 +0000

    vpc

---
+++ terraform/vpc/yngmind_apnortheast2/backend.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_version = ">= 1.5.7"
+
+  backend "s3" {
+    bucket         = "yngmin-apnortheast2-tfstate"
+    key            = "yngmin/terraform/vpc/yngmind_apnortheast2/terraform.tfstate"
+    region         = "ap-northeast-2"
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
+  }
+}
+
